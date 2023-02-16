f0eb55db029d04a2b53648056322350e90f04cb4
Mikolaj Mikolajewski <mikolaj.jewski@gmail.com>
2023-02-16 19:31:18 +0000

    Rozwiązanie 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,18 @@
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+
 public class Main {
 
-
     public static void main(String[] args) {
+        String fileName = "numbers.txt";
+        try {
+            Map<Integer, Integer> mapFromFile = NumbersReader.getListFromFile(fileName);
+            NumbersReader.printStats(mapFromFile);
 
+        } catch (FileNotFoundException e) {
+            System.out.println("Nie udało się wczytać pliku");
+        }
     }
-
 }
